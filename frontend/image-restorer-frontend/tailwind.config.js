4649193530b0d79738f4084e4ee6a0ddf9b73f19
hongjr03 <hongjr03@gmail.com>
2024-11-26 21:18:56 +0800

    feat: 添加 vue-icon 依赖并更新相关配置，优化 Tailwind CSS 配置

--- frontend/image-restorer-frontend/tailwind.config.js
+++ frontend/image-restorer-frontend/tailwind.config.js
@@ -1,16 +1,18 @@
-module.exports = {
+import daisyui from 'daisyui';
+
+export default {
   content: [
     "./index.html", // 指定 index.html
     "./src/**/*.{vue,js,ts,jsx,tsx}", // 指定 src 文件夹内的所有 .vue, .js, .ts, .jsx, 和 .tsx 文件
   ],
   theme: {
-  extend: {}, // 可以在这里扩展自定义主题
-},
-plugins: [
-  require('daisyui'),
-], // 可以在这里添加 Tailwind 插件
-    daisyui: {
-  themes: ["light", "dark"],
-  // 配置 daisyUI 主题
-}
+    extend: {}, // 可以在这里扩展自定义主题
+  },
+  plugins: [
+    daisyui,
+  ], // 可以在这里添加 Tailwind 插件
+  daisyui: {
+    themes: ["light", "dark"],
+    // 配置 daisyUI 主题
+  }
 };
